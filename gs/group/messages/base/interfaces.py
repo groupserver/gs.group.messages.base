811c0d7d887eee054d09465d87e8aeabc4609bbe
Michael JasonSmith <mpj17@onlinegroups.net>
2013-01-10 15:32:10 +1100

    Taking over the responsibility for the Messages tabs from the group homepage.

---
+++ gs/group/messages/base/interfaces.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+from zope.viewlet.interfaces import IViewletManager
+
+
+class IGroupHomepageMessages(IViewletManager):
+    '''A viewlet manager for the messages on the group homepage'''
