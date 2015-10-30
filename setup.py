2c7560590424f614e7a326afdab3122256e3c558
Michael JasonSmith <mpj17@onlinegroups.net>
2015-10-30 11:07:28 -0700

    Updating the meatadata

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ import os
 from setuptools import setup, find_packages
 from version import get_version
 
+name = 'gs.group.messages.base'
 version = get_version()
 
 with codecs.open('README.rst', encoding='utf-8') as f:
@@ -28,7 +29,7 @@ with codecs.open(os.path.join("docs", "HISTORY.rst"),
     long_description += '\n' + f.read()
 
 setup(
-    name='gs.group.messages.base',
+    name=name,
     version=version,
     description="Base code for messages in a GroupServer group",
     long_description=long_description,
@@ -46,10 +47,11 @@ setup(
     keywords='groupserver message',
     author='Michael JasonSmith',
     author_email='mpj17@onlinegroups.net',
-    url='https://github.com/groupserver/gs.group.messages.base',
+    url='https://github.com/groupserver/{0}'.format(name),
     license='ZPL 2.1',
     packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['gs', 'gs.group', 'gs.group.messages'],
+    namespace_packages=['.'.join(name.split('.')[:i])
+                        for i in range(1, len(name.split('.')))],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
@@ -63,6 +65,7 @@ setup(
         'gs.search.base',
         'gs.viewlet',
     ],
+    test_suite="{0}.tests.test_all".format(name),
     entry_points="""
     # -*- Entry points: -*-
     """,)
